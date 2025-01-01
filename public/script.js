6973a0fe239aed22c939491385504c3d04693114
onur2025 <yassin.onur@gmail.com>
2025-01-01 23:48:02 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,29 @@
+const showNotification = (message) => {
+  const notification = document.createElement('div');
+  notification.textContent = message;
+  notification.style.backgroundColor = '#007bff';
+  notification.style.color = '#fff';
+  notification.style.padding = '10px';
+  notification.style.marginTop = '10px';
+  notification.style.borderRadius = '4px';
+  notification.style.textAlign = 'center';
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 5000);
+};
+
+
 const socket = io('https://call-app-n3pl.onrender.com'); // رابط الخادم الخاص بك
 
 document.getElementById('registerBtn').addEventListener('click', () => {
   const userId = document.getElementById('userId').value;
   if (userId) {
     socket.emit('register', { userId });
-    alert('Registered successfully!');
+   showNotification('Registered successfully!');
   } else {
-    alert('Please enter your ID.');
+    showNotification('Please enter your ID.');
   }
 });
 
@@ -15,12 +32,12 @@ document.getElementById('callBtn').addEventListener('click', () => {
   const calleeId = document.getElementById('calleeId').value;
   if (userId && calleeId) {
     socket.emit('call', { callerId: userId, calleeId });
-    alert(`Calling ${calleeId}...`);
+    showNotification(`Calling ${calleeId}...`);
   } else {
-    alert('Please fill in both IDs.');
+    showNotification('Please fill in both IDs.');
   }
 });
 
 socket.on('incoming_call', ({ callerId }) => {
-  alert(`Incoming call from: ${callerId}`);
+  showNotification(`Incoming call from: ${callerId}`);
 });
