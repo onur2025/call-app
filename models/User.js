9944dc07aa6f024a0db9a459af90d3319393a210
onur2025 <yassin.onur@gmail.com>
2025-01-03 01:27:48 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // يمكنك تحسينها لاحقًا لتشمل التشفير
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: false, unique: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
-
