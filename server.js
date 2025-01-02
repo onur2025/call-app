34381c9282e25a8dabf75c88c7dabe74d95d4f8f
onur2025 <yassin.onur@gmail.com>
2025-01-02 18:41:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,55 +23,40 @@ io.on('connection', (socket) => {
     console.log(`Call initiated by ${callerId} to ${calleeId}`);
     const calleeSocket = users[calleeId];
     if (calleeSocket) {
-      console.log(`Notifying ${calleeId} of incoming call from ${callerId}`);
       io.to(calleeSocket).emit('incoming_call', { callerId, calleeId });
     } else {
-      console.log(`User ${calleeId} is unavailable`);
       socket.emit('user_unavailable');
     }
   });
 
   // قبول المكالمة
   socket.on('accept_call', ({ callerId }) => {
-    console.log(`Call accepted by ${socket.id} for caller ${callerId}`);
     const callerSocket = users[callerId];
     if (callerSocket) {
-      console.log(`Notifying ${callerId} to redirect to call.html`);
       io.to(callerSocket).emit('redirect_to_call');
-    } else {
-      console.log(`Caller ${callerId} is not connected.`);
     }
   });
 
   // رفض المكالمة
   socket.on('reject_call', ({ callerId }) => {
-    console.log(`Call rejected by ${socket.id} for caller ${callerId}`);
     const callerSocket = users[callerId];
     if (callerSocket) {
-      console.log(`Notifying ${callerId} that call was rejected`);
       io.to(callerSocket).emit('call_rejected');
     }
   });
 
   // إنهاء المكالمة
   socket.on('end_call', ({ otherUserId }) => {
-    console.log(`End call event received. Other user ID: ${otherUserId}`);
     const otherUserSocket = users[otherUserId];
     if (otherUserSocket) {
-      console.log(`Notifying user ${otherUserId} about call ended.`);
       io.to(otherUserSocket).emit('call_ended');
-    } else {
-      console.log(`User ${otherUserId} not found or disconnected.`);
-      socket.emit('user_unavailable', { message: 'The other user is not available.' });
     }
   });
 
-  // عند فصل المستخدم
+  // فصل المستخدم
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
     for (const [userId, socketId] of Object.entries(users)) {
       if (socketId === socket.id) {
-        console.log(`Removing user ${userId} due to disconnection.`);
         delete users[userId];
         break;
       }
