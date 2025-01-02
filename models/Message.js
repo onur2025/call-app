df2dedffcabdd835ae139893b33ab967e3390174
onur2025 <yassin.onur@gmail.com>
2025-01-03 00:51:46 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  recipient: { type: String, required: true },
+  content: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
 
