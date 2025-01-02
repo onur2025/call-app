0e752cf3ea76c15b03eb43cdd5048e520440a166
onur2025 <yassin.onur@gmail.com>
2025-01-03 02:11:20 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
-  sender: { type: String, required: true },
-  recipient: { type: String, required: true },
-  content: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: false, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Message', messageSchema);
+module.exports = mongoose.model('User', userSchema);
