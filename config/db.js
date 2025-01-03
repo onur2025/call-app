37a8d2985b324d1100d8944cabf2183434966469
onur2025 <yassin.onur@gmail.com>
2025-01-03 19:28:14 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,21 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+    const db = client.db("chatdatabase"); // استبدل باسم قاعدة البيانات
+    return db;
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectToDatabase;
 
